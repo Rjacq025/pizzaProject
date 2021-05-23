99ab105fc0ecfa4b0263ac3bc51b77111639c677
Rjacq025 <73442351+Rjacq025@users.noreply.github.com>
2021-05-22 22:27:23 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// Richardson Jacques
+//This program is a pizza ordering simulator for my Integration Project!
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Jacques Pizzeria! May I take your order?");
+
+	}
+
+}
